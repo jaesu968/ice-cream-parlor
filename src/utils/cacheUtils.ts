0d487b215a368606dc79545595e1a910e6c51ae2
jaesu968 <mazterk968@gmail.com>
2025-10-21 14:23:34 -0500

    create Cached data functionality

--- src/utils/cacheUtils.ts
+++ src/utils/cacheUtils.ts
@@ -1,5 +1,20 @@
 import AsyncStorage from '@react-native-async-storage/async-storage';
 
+/**
+ * 
+ * create cached data
+ * 3 parameters: key, data, expiration time in minutes (default 30 minutes)
+ */
+export const creatCache = async (key: string, data: any, minutes: number = 30) => {
+ // store data in AsyncStorage with expiration timestamps
+  const cacheEntry = {
+    data,
+    expires: Date.now() + minutes * 60 * 1000 // expiration time in milliseconds
+  };
+  // store the cache entry in AsyncStorage using the cache:${key} format
+  await AsyncStorage.setItem(`cache:${key}`, JSON.stringify(cacheEntry));
+}
+
 
 
 /**

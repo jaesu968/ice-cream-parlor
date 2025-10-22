015d4c1442cb76d57376dcf1a08978d678ba35f0
jaesu968 <mazterk968@gmail.com>
2025-10-22 09:42:31 -0500

    network Utilties updated for error handling

--- src/utils/networkUtils.ts
+++ src/utils/networkUtils.ts
@@ -1,15 +1,25 @@
+import { get } from "mongoose";
+
 export const fetchWithRetry = async (url: string, retries = 3): Promise<Response> => {
   
   // add a loop here
   for (let attempt = 0; attempt < retries; attempt++) {
     // try to find the response via fetch 
     try {
+      // fetch the url and store the response in a constant variable called response
       const response = await fetch(url); 
       // if the response is ok return it 
-      if (response.ok) return response; 
+      if (response.ok) {
+        return(response); 
+      } else {
+        // if it is a client error (4xx), do not retry, throw an error immediately
+        if (response.status >= 400 && response.status < 500) {
+          throw new Error(getErrorMessage(response.status));
+        }
+      } 
     } catch (error){
-      // if an error occurs, throw it
-      if (attempt === retries  - 1) throw error; 
+      // if the last attempt has been reached, rethrow the error
+      if (attempt === retries  - 1) throw error;
       // otherwise, await a new Promise that resolves after a delay before retrying
       await new Promise(resolve => setTimeout(resolve, 1000 * Math.pow(2, attempt))); // exponential backoff
     }
